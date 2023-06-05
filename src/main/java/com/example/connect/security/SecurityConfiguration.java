b04c99b27c15e8849b4449b8a72fa75eb6becdde
Obinna Umerah <umerahobinna@gmail.com>
2023-06-04 21:37:45 -0500

    Added WebSecurityCustomizer

--- src/main/java/com/example/connect/security/SecurityConfiguration.java
+++ src/main/java/com/example/connect/security/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -71,6 +72,11 @@ public class SecurityConfiguration {
     public MyUserDetails myUserDetails() {
         return (MyUserDetails) SecurityContextHolder.getContext().getAuthentication()
                 .getPrincipal();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**");
     }
 
 }

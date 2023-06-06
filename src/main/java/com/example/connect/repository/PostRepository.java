1fe1588f4c1e34b9c8976ab5084fcd26a2c31d28
Obinna Umerah <umerahobinna@gmail.com>
2023-06-06 03:29:58 -0500

    Created PostRepository class

---
+++ src/main/java/com/example/connect/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.example.connect.repository;
+
+import com.example.connect.model.Post;
+import com.example.connect.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PostRepository extends JpaRepository<User, Long> {
+}

e0195ca2b99d06e1b1e980df2bdb129426cf2329
Obinna Umerah <umerahobinna@gmail.com>
2023-06-06 03:33:11 -0500

    Added methods for testings

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -2,8 +2,11 @@ package definitions;
 
 import com.example.connect.ConnectApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONException;
@@ -14,6 +17,9 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ConnectApplication.class)
@@ -28,25 +34,25 @@ public class SpringBootCucumberTestDefinitions {
     public String getSecurityKey() throws Exception{
         RequestSpecification request = RestAssured.given();
         JSONObject requestBody = new JSONObject();
-        requestBody.put("email", "ruby@gmail.com");
-        requestBody.put("password", "123456");
+        requestBody.put("email", "jack@aol.com");
+        requestBody.put("password", "jack123");
         request.header("Content-Type", "application/json");
         return response.jsonPath().getString("message");
     }
 
-    @Given("That an agent is able to register")
-    public void thatAnAgentIsAbleToRegister() throws Exception{
+    @Given("That a user is able to register")
+    public void thatAUserIsAbleToRegister() throws Exception{
         try {
 
             JSONObject requestBody = new JSONObject();
-            requestBody.put("password", "123456");
-            requestBody.put("email", "email@mail.com");
+            requestBody.put("password", "jack123");
+            requestBody.put("emailAddress", "jack@aol.com");
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             HttpEntity<String> request = new HttpEntity<String>(requestBody.toString(), headers);
 
-            ResponseEntity<String> response = new RestTemplate().exchange(BASE_URL + port + "/auth/register/", HttpMethod.POST, request, String.class);
+            ResponseEntity<String> response = new RestTemplate().exchange(BASE_URL + port + "/auth/users/register/", HttpMethod.POST, request, String.class);
 
             Assert.assertEquals(response.getStatusCode(), HttpStatus.CREATED);
         } catch (HttpClientErrorException e) {
@@ -55,6 +61,41 @@ public class SpringBootCucumberTestDefinitions {
             throw new RuntimeException(e);
         }
     }
+
+//    @Given("A list of properties are available")
+//    public void aListOfPostsAreAvailable() {
+//        try {
+//            ResponseEntity<String> response = new RestTemplate().exchange(BASE_URL + port + "/api/users/", HttpMethod.GET, null, String.class);
+//            List<Map<String, String>> properties = JsonPath
+//                    .from(String.valueOf(response
+//                            .getBody()))
+//                    .getList("$");
+//            Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+//            Assert.assertTrue(properties.size() > 0);
+//        } catch (HttpClientErrorException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @When("I login to my account")
+//    public void iLoginToMyAccount() throws JSONException {
+//        RestAssured.baseURI = BASE_URL;
+//        RequestSpecification request = RestAssured.given();
+//        JSONObject requestBody = new JSONObject();
+//        requestBody.put("password", "jack123");
+//        requestBody.put("email", "jack@aol.com");
+//        request.header("Content-Type", "application/json");
+//        response = request.body(requestBody.toString()).post(BASE_URL + port + "/auth/users/login/");
+//    }
+//
+//    @Then("JWT key is returned")
+//    public void jwtKeyIsReturned() {
+//        Assert.assertEquals(200, response.getStatusCode());
+//        Assert.assertNotNull(response.body());
+//    }
+
+
+
 
 
 
--- src/test/resources/features/SpringBootCucumberTestFeature.feature
+++ src/test/resources/features/SpringBootCucumberTestFeature.feature
@@ -1,6 +1,10 @@
 Feature: Rest API functionalites
 
   Scenario: User is able to register and login
-    Given That an agent is able to register
-    When I login to my account
-    Then JWT key is returned
+    Given That a user is able to register
+#    When I login to my account
+#    Then JWT key is returned
+
+    Scenario: User is able to all of the post's features
+      Given That a user is able to post
+#      When I make a post

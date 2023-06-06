0db168efa480261ece13b3d65afe7f5965241263
Obinna Umerah <umerahobinna@gmail.com>
2023-06-06 03:37:51 -0500

    Created users and posts in the dataloader class

--- src/main/java/com/example/connect/seed/DataLoader.java
+++ src/main/java/com/example/connect/seed/DataLoader.java
@@ -1,6 +1,8 @@
 package com.example.connect.seed;
 
+import com.example.connect.model.Post;
 import com.example.connect.model.User;
+import com.example.connect.repository.PostRepository;
 import com.example.connect.repository.UserRepository;
 import com.example.connect.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,9 @@ public class DataLoader implements CommandLineRunner {
     UserRepository userRepository;
 
     @Autowired
+    PostRepository postRepository;
+
+    @Autowired
     UserService userService;
 
 
@@ -25,11 +30,17 @@ public class DataLoader implements CommandLineRunner {
     private void loadUserData() {
         if (userRepository.count() == 0) {
             User user1 = new User(1L, "jack@aol.com", "jack123");
-            User user2 = new User(2L, "rachel", "rachel123");
-            User user3 = new User(3L, "Liane", "liane123");
+            User user2 = new User(2L, "ron@aol.comm", "ron123");
+            User user3 = new User(3L, "luke@aol.com", "luke123");
             userService.createUser(user1);
             userService.createUser(user2);
             userService.createUser(user3);
+
+            Post post = new Post(1L, "Hello Peeps!");
+            Post post2 = new Post(2L,"What's for dinner?");
+            Post post3 = new Post(3L, "This is war, get up and fight.");
+
+            post
         }
     }
 }

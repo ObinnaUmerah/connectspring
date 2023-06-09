593dfa4b6c4c4b56000ab8b293ac93f3d9a4df01
Obinna Umerah <umerahobinna@gmail.com>
2023-06-09 09:43:04 -0500

    Updated the PostController class.

--- src/main/java/com/example/connect/controller/PostController.java
+++ src/main/java/com/example/connect/controller/PostController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path="/api/")
+@RequestMapping(path="/api")
 public class PostController {
 
     private PostService postService;

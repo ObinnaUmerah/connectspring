232b87cc4222bb4ef9aea02ab2ec72a0bc1c3647
Obinna Umerah <umerahobinna@gmail.com>
2023-06-12 10:11:27 -0500

    Added documentation

--- src/main/java/com/example/connect/ConnectApplication.java
+++ src/main/java/com/example/connect/ConnectApplication.java
@@ -1,3 +1,6 @@
+/**
+ * This is the main class for the Connect application.
+ */
 package com.example.connect;
 
 import org.springframework.boot.SpringApplication;
@@ -6,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConnectApplication {
 
+	/**
+	 * The main method that starts the Connect application.
+	 *
+	 * @param args The command-line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ConnectApplication.class, args);
 	}

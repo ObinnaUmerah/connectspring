ad7c392050e30419199fd5a8a27df10fe5c57956
Obinna Umerah <umerahobinna@gmail.com>
2023-06-12 10:11:59 -0500

    Added documentation

--- src/main/java/com/example/connect/service/UserService.java
+++ src/main/java/com/example/connect/service/UserService.java
@@ -1,3 +1,6 @@
+/**
+ * This class represents a service for managing users in a social media application.
+ */
 package com.example.connect.service;
 
 import com.example.connect.exception.InformationExistException;
@@ -22,11 +25,19 @@ public class UserService {
 
     private UserRepository userRepository;
     private AuthenticationManager authenticationManager;
-
     private JWTUtils jwtUtils;
     private MyUserDetails myUserDetails;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Constructs a UserService instance.
+     *
+     * @param userRepository        The user repository.
+     * @param passwordEncoder       The password encoder.
+     * @param jwtUtils              The JWT utility class.
+     * @param authenticationManager The authentication manager.
+     * @param myUserDetails         The custom user details implementation.
+     */
     @Autowired
     public UserService(UserRepository userRepository,
                        @Lazy PasswordEncoder passwordEncoder,
@@ -40,24 +51,41 @@ public class UserService {
         this.myUserDetails = myUserDetails;
     }
 
+    /**
+     * Creates a new user.
+     *
+     * @param userObject The user object to create.
+     * @return The created user.
+     * @throws InformationExistException if a user with the same email address already exists.
+     */
     public User createUser(User userObject) {
         if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
             userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
             return userRepository.save(userObject);
         } else {
-            throw new InformationExistException("user with email address " + userObject.getEmailAddress() +
+            throw new InformationExistException("User with email address " + userObject.getEmailAddress() +
                     " already exists");
         }
     }
 
+    /**
+     * Finds a user by email address.
+     *
+     * @param email The email address of the user to find.
+     * @return The user with the specified email address.
+     */
     public User findUserByEmailAddress(String email) {
         return userRepository.findUserByEmailAddress(email);
     }
 
-
+    /**
+     * Authenticates a user and generates a JWT token.
+     *
+     * @param loginRequest The login request containing the user's email and password.
+     * @return A response entity containing the generated JWT token.
+     */
     public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
         try {
-            System.out.println(loginRequest.getPassword() + loginRequest.getEmail());
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
             SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -66,7 +94,7 @@ public class UserService {
             final String JWT = jwtUtils.generateJwtToken(myUserDetails);
             return ResponseEntity.ok(new LoginResponse(JWT));
         } catch (Exception e) {
-            return ResponseEntity.ok(new LoginResponse("Error : username or password is incorrect"));
+            return ResponseEntity.ok(new LoginResponse("Error: username or password is incorrect"));
         }
     }
 }
